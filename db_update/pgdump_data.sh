1d08248eedfb7fb83e2352b704f6d94a01af163b
Julian Käflein <jkaeflein@geomer.de>
2020-10-06 18:52:42 +0200

    Add data dump function

---
+++ db_update/pgdump_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+NEW_STRUCT=`ls -t db_structure | head -n1`
+NEW_STRUCT_TS=`stat -c "%Y" db_structure/$NEW_STRUCT`
+
+LATEST_BK=`ls -t db_backup | head -n1`
+LATEST_BK_TS=`stat -c "%Y" db_backup/$LATEST_BK`
+
+if [ ${NEW_STRUCT_TS} -gt ${LATEST_BK_TS} ] ; then
+
+    OUTFILE=backup_$(date "+%Y%m%d_%H%M%S")
+    echo "dumping latest database state to $OUTFILE"
+    
+    if PGPASSWORD=$POSTGRES_INIT_PASSWORD pg_dump -s -h db -U postgres -d sauber_data > db_backup/$OUTFILE.sql; then
+        if PGPASSWORD=$POSTGRES_INIT_PASSWORD psql -h db -U postgres -d sauber_data -c 'SELECT timescaledb_pre_restore();' ; then 
+                if PGPASSWORD=$POSTGRES_INIT_PASSWORD psql -h db -U postgres < db_structure/$NEW_STRUCT ; then
+                    if PGPASSWORD=$POSTGRES_INIT_PASSWORD psql -h db -U postgres -d sauber_data -c 'SELECT timescaledb_pre_restore();' ; then
+                        echo 'Finished updating new database structure'
+                    else echo 'Warning: TimscaleDB-post-restore not set'; fi
+                else echo 'Error restoring databases'; fi
+        else echo "Error: Could not set TimescaleDB to restore mode."; fi
+    else echo "Error: Could not backup DB structure."; fi
+else echo "Error: New DB structure file is older than latest Database dump. Check file or update creation time."; fi
